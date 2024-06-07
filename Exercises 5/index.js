a17c7d21d220aed38019b7c8e28cfdb1d8d53990
BismaArshad <164316452+BBismaarshad@users.noreply.github.com>
2024-06-07 12:00:27 +0500

    push my TypeScript 45 Questions to github

---
+++ Exercises 5/index.js
@@ -0,0 +1,5 @@
+let personName = "\t\n Bisma \n\t";
+console.log(personName);
+let strippingName = personName.trim();
+console.log(strippingName);
+export {};
---
+++ Exercises 5/index.ts
@@ -0,0 +1,4 @@
+let personName = "\t\n Bisma \n\t";
+console.log(personName);
+let strippingName = personName.trim();
+console.log(strippingName);
