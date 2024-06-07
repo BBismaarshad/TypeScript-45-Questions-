83ea1c6e287059180c2d83aa29b8b6df45c3fa3b
BismaArshad <164316452+BBismaarshad@users.noreply.github.com>
2024-06-07 12:11:31 +0500

    push my Typescript 45 Quesstions to github

---
+++ Exercises6/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Addition: ", 2 + 6);
+console.log("Subtraction: ", 10 - 2);
+console.log("Multiplication: ", 2 * 4);
+console.log("Divison: ", 16 / 2);
---
+++ Exercises6/index.ts
@@ -0,0 +1,8 @@
+
+console.log("Addition: ",2+6);
+console.log("Subtraction: ",10-2);
+console.log("Multiplication: ",2*4);
+console.log("Divison: ",16/2);
+
+
+
