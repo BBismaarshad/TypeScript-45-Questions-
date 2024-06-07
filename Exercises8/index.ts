78aa36a3ee83c3f2b435bfd3c5d3171230493028
BismaArshad <164316452+BBismaarshad@users.noreply.github.com>
2024-06-07 12:24:15 +0500

    push my Typescript 45 Questions to github

---
+++ Exercises8/index.ts
@@ -0,0 +1,29 @@
+//program 1 :
+
+// Author : Bisma 
+// Date : 2024-5-25
+
+//This program prints the result of basic arithmetic operations that equal 8
+
+console.log("Addition:",2+6);   //output 8
+console.log("Subtraction:",10-2);   //output 8
+console.log("Multiplication:",2*4);  //output 8
+console.log("Division:",16/2);   //output 8
+
+
+//program
+
+//Author : Bisma
+//Date : 2024-5-25
+
+//This program prints a message revealing my favorite number .
+
+let favoriteNumber : number = 2; // replsce 2 with my favorite number
+
+let message : string = (`My favorite number is ${favoriteNumber}`);
+console.log(message);    //output My favorit number is 2
+
+
+
+
+
