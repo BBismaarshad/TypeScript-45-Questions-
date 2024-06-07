73402bf55b7e408bf8b3bbef5a66c993b7940f1c
BismaArshad <164316452+BBismaarshad@users.noreply.github.com>
2024-06-07 08:33:23 +0500

    push my Typescript 45 Questions to github

---
+++ Exercises 4/index.js
@@ -0,0 +1,5 @@
+let famous_person = "Albert Einstein";
+let quote = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, progress, giving birth to evolution.";
+let message = (`${famous_person} once saide, ${quote}`);
+console.log(message);
+export {};
---
+++ Exercises 4/index.ts
@@ -0,0 +1,12 @@
+let famous_person: string = "Albert Einstein";
+
+let quote: string = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, progress, giving birth to evolution.";
+
+let message: string = (`${famous_person} once saide, ${quote}`)
+
+console.log(message);
+
+
+
+
+
